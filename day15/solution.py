c43af066f825cdaa3f35881268054e5875212239
Simon Compston <compston@gmail.com>
2025-12-04 19:15:17 +0000

    day 15

--- day15/solution.py
+++ day15/solution.py
@@ -13,6 +13,7 @@ class Solution:
             lines = f.readlines()
             for line in lines:
                 if line.startswith('#'):
+                    line = line.replace('#', '##').replace('.', '..').replace('O', '[]').replace('@', '@.')
                     self.grid.append(list(line.strip()))
                 elif len(line) > 0:
                     self.moves += line.strip()
@@ -57,9 +58,56 @@ class Solution:
                 #print(f"transformed row copy is {"".join(row)}")
                 self.grid[y] = row
                 self.x = self.x + dir
+                #self.print_grid()
                 break
             x = x + dir
             c = self.grid[y][x]
+
+
+    def copy_grid(self) -> list:
+        new_grid = []
+        for y in range(len(self.grid)):
+            new_grid.append(self.grid[y].copy())
+        return new_grid
+    
+
+    def move_boxes_vertically(self, dir) -> bool:
+        #print(f"would move boxes {dir}: from [{self.x,self.y}]")
+        connected = []
+        can_move = self.connected_boxes(self.x, self.y + dir, dir, connected)
+        #print(f"got connected boxes {connected} and can move is {can_move}")
+        if not can_move:
+            return False
+        
+        new_grid = self.copy_grid()
+        for point in connected:
+            new_grid[point[1]][point[0]] = '.'
+        for point in connected:
+            x = point[0]
+            y = point[1]
+            c = self.grid[y][x]
+            #print(f"  working on connected point {point} with {c} -> new point is [{x},{y+dir}]")
+            new_grid[y+dir][x] = c
+        self.grid = new_grid
+        return True
+    
+
+    def connected_boxes(self, x, y, dir, connected) -> bool:
+        c = self.grid[y][x]
+        if c == '#':
+            return False
+        elif c == '.':
+            return True
+        elif c == '[':
+            connected.append([x,y])
+            connected.append([x+1,y])
+            return self.connected_boxes(x, y+dir, dir, connected) and self.connected_boxes(x+1, y+dir, dir, connected)
+        elif c == ']':
+            connected.append([x,y])
+            connected.append([x-1,y])
+            return self.connected_boxes(x, y+dir, dir, connected) and self.connected_boxes(x-1, y+dir, dir, connected)
+
+        raise Exception(f"unexpected character at [{x},{y}] - {c}")
 
 
     def move_vertical(self, dir):
@@ -70,14 +118,13 @@ class Solution:
         while c != '#' and self.inbounds(x, y):
             #print(f"next grid point at [{x},{y}] is {c}")
             if c == '.':
-                if dir == -1:
-                    for i in range(self.y - y):
-                        self.grid[y+i][x] = self.grid[y+i+1][x]
-                else:
-                    for i in range(y - self.y):
-                        self.grid[y-i][x] = self.grid[y-i-1][x]
-                self.grid[self.y][x] = '.'
-                self.y = self.y + dir
+                moved = True
+                if abs(self.y - y) > 1:
+                    moved = self.move_boxes_vertically(dir)
+                if moved:
+                    self.grid[self.y + dir][x] = self.grid[self.y][x]
+                    self.grid[self.y][x] = '.'
+                    self.y = self.y + dir
                 #self.print_grid()
                 break
             y = y + dir
@@ -94,7 +141,6 @@ class Solution:
                 self.move_vertical(-1)
             else:
                 self.move_vertical(1)
-            #print(f"after {m} move")
         self.print_grid()
 
     
@@ -103,14 +149,14 @@ class Solution:
         for y in range(len(self.grid)):
             for x in range(len(self.grid[0])):
                 c = self.grid[y][x]
-                if c == 'O':
+                if c == '[':
                     cost += (100 * y) + x
         return cost
 
 
     def solve(self, filename):
         self.read_file(filename)
-        print(f"grid is {self.grid}")
+        self.print_grid()
         print(f"moves are [{self.moves}]")
         self.find_starting()
         self.move()
