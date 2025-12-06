6584870ba22ac5284728bebe7c1ae53d9ffad4d2
vleurgat <compston@gmail.com>
2025-12-06 12:24:34 +0000

    day 17

--- day17/solution.py
+++ day17/solution.py
@@ -1,4 +1,5 @@
 import math
+import sys
 
 class Solution:
 
@@ -57,49 +58,49 @@ class Solution:
                 com = self.combo(i+1)
                 den = pow(2, com)
                 res = math.floor(self.a / den)
-                print(f"adv({opc}) --> floor( a({self.a}) / pow(2, combo({self.program[i+1]}=={com})=={den} ) == {res}")
+                #print(f"adv({opc}) --> floor( a({self.a}) / pow(2, combo({self.program[i+1]}=={com})=={den} ) == {res}")
                 self.a = res
                 return i+2
             case 1: # bxl
                 res = self.b ^ self.operand(i+1)
-                print(f"bxl({opc}) -> b({self.b}) ^ lit({self.program[i+1]}) == {res}")
+                #print(f"bxl({opc}) -> b({self.b}) ^ lit({self.program[i+1]}) == {res}")
                 self.b = res
                 return i+2
             case 2: # bst
                 com = self.combo(i+1)
                 res = com % 8
-                print(f"bst({opc}) -> combo({self.program[i+1]}=={com})%8 == {res}")
+                #print(f"bst({opc}) -> combo({self.program[i+1]}=={com})%8 == {res}")
                 self.b = res
                 return i+2
             case 3: # jnz
                 if self.a == 0:
-                    print(f"jnz({opc}) a==0 --> do nothing but skip operand {self.program[i+1]}")
+                    #print(f"jnz({opc}) a==0 --> do nothing but skip operand {self.program[i+1]}")
                     return i+2
                 res = self.operand(i+1)
-                print(f"jnz({opc}) --> jump to {self.program[i+1]} == {res}")
+                #print(f"jnz({opc}) --> jump to {self.program[i+1]} == {res}")
                 return res
             case 4: # bxc
                 res = self.b ^ self.c
-                print(f"bxc({opc}) --> b({self.b}) XOR c({self.c}) == {res}")
+                #print(f"bxc({opc}) --> b({self.b}) XOR c({self.c}) == {res}")
                 self.b = res
                 return i+2
             case 5: # out
                 res = self.combo(i+1) % 8
-                print(f"out({opc}) --> combo({self.program[i+1]})%8 == {res}")
+                #print(f"out({opc}) --> combo({self.program[i+1]})%8 == {res}")
                 self.output.append(res)
                 return i+2
             case 6: # bdv
                 com = self.combo(i+1)
                 den = pow(2, com)
                 res = math.floor(self.a / den)
-                print(f"bdv({opc}) --> floor( a({self.a}) / pow(2, combo({self.program[i+1]}=={com})=={den} ) == {res}")
+                #print(f"bdv({opc}) --> floor( a({self.a}) / pow(2, combo({self.program[i+1]}=={com})=={den} ) == {res}")
                 self.b = res
                 return i+2
             case 7: # cdv
                 com = self.combo(i+1)
                 den = pow(2, com)
                 res = math.floor(self.a / den)
-                print(f"cdv({opc}) --> floor( a({self.a}) / pow(2, combo({self.program[i+1]}=={com})=={den} ) == {res}")
+                #print(f"cdv({opc}) --> floor( a({self.a}) / pow(2, combo({self.program[i+1]}=={com})=={den} ) == {res}")
                 self.c = res
                 return i+2
             case _:
@@ -112,29 +113,34 @@ class Solution:
             opc = self.program[i]
             i = self.opcode(opc, i)
             if self.output != self.program[:len(self.output)]:
-                print(f"output is going wrong: {self.output} != {self.program[:len(self.output)]}")
+                #print(f"output is going wrong: {self.output} != {self.program[:len(self.output)]}")
                 return
 
 
-    def solve(self, filename):
+    def solve(self, filename, start, inc):
         self.read_file(filename)
+        print(f"starting at {start} incrementing by {inc}")
         print(f"state: {self}")
         print(f"program: {self.program}")
-        for i in range(1,100):
-            if i%8 != 7:
-                continue
+        i = start
+        while True:
             self.a = i
             self.b = 0
             self.c = 0
             self.output = []
             self.execute()
-            if i % 1000000 == 0:
-                print(f"at i={i}\nstate is: {self}")
+            if i % 10000000 == 0:
+                print(f"{start} at i={i}\nstate is: {self}")
             if self.output == self.program:
-                print(f"got a matching output for i={i}")
+                print(f"{start} got a matching output for i={i}")
                 return
-
+            i += inc
 
 if __name__ == "__main__":
     solution = Solution()
-    solution.solve('input.txt')
+    start = 0
+    inc = 1
+    if len(sys.argv) > 2:
+        start = int(sys.argv[1])
+        inc = int(sys.argv[2])
+    solution.solve('input.txt', start, inc)
